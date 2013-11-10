3c3434cab89accfffadc9b5c2da6b88998f72e6b
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-11-10 08:23:04 +0000

    shell implementation of named groups

--- lib/implements.rb
+++ lib/implements.rb
@@ -10,15 +10,18 @@ module Implements
     end
 
     def self.extended(base)
-      base.instance_variable_set(:@implementations, [])
+      hsh = Hash.new { |hash, key| hash[key] = [] }
+      base.instance_variable_set(:@implementations, hsh)
     end
 
     def register(implementation, options, &block)
-      @implementations << [implementation, options, block]
+      [:auto, options[:names]].flatten.compact.each do |group|
+        @implementations[group] << [implementation, options, block]
+      end
     end
 
     def new(*args, &block)
-      @implementations.reverse_each do |implementation, options, check|
+      @implementations[:auto].reverse_each do |implementation, options, check|
         unless check && !check.call(*args, &block)
           return implementation.new(*args, &block)
         end
@@ -37,9 +40,15 @@ module Implements
     def implements(interface, options = {}, &block)
       fail TypeError unless interface.kind_of?(Interface)
 
-      interface.register(self, options.dup, &block)
+      names = options.fetch(:as) { implementation_descriptors(interface) }
+      interface.register(self, names: Array(names), &block)
 
       include interface
+    end
+
+    def implementation_descriptors(interface)
+      name = self.name
+      name && name.sub(Regexp.new("^#{interface}::"), '')
     end
   end
 end
