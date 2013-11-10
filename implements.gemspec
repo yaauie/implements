5eb2af35bf57bce28e1c610de931721d015cde7c
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-11-10 07:56:03 +0000

    base implementation, interfaces and implementations oh my!

--- implements.gemspec
+++ implements.gemspec
@@ -27,6 +27,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.4.0.rc.1'
   spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec', '~> 2.14'
+
 
   # code quality
   spec.add_development_dependency 'ruby-appraiser-reek'
--- lib/implements.rb
+++ lib/implements.rb
@@ -3,4 +3,43 @@ require 'implements/version'
 
 # Tools for Interfaces.
 module Implements
+  # Interface: mix into your interfaces.
+  module Interface
+    def self.included(base)
+      base && fail(ScriptError, "#{self} supports only extend, not include.")
+    end
+
+    def self.extended(base)
+      base.instance_variable_set(:@implementations, [])
+    end
+
+    def register(implementation, options, &block)
+      @implementations << [implementation, options, block]
+    end
+
+    def new(*args, &block)
+      @implementations.reverse_each do |implementation, options, check|
+        unless check && !check.call(*args, &block)
+          return implementation.new(*args, &block)
+        end
+      end
+      fail(Implementation::NotFound, "#{self}: no compatible implementation.")
+    end
+  end
+
+  # Implementation: mix into your implementations
+  module Implementation
+    # An exception raised when an implementation cannot be found.
+    NotFound = Class.new(NotImplementedError)
+
+    private
+
+    def implements(interface, options = {}, &block)
+      fail TypeError unless interface.kind_of?(Interface)
+
+      interface.register(self, options.dup, &block)
+
+      include interface
+    end
+  end
 end
